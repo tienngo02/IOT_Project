4e0bf5738ebb8a9aa60f0418c686e0ca91429a7e
MSI\nvtie <tien.ngo30082002@hcmut.edu.vn>
2024-04-09 23:06:13 +0700

    Test sensor and actuator

--- rs485.py
+++ rs485.py
@@ -1,7 +1,20 @@
-print("Sensors and Actuators")
-
 import time
 import serial.tools.list_ports
+
+def addModbusCrc(msg):
+    crc = 0xFFFF
+    for n in range(len(msg)):
+        crc ^= msg[n]
+        for i in range(8):
+            if crc & 1:
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    ba = crc.to_bytes(2, byteorder='little')
+    msg.append(ba[0])
+    msg.append(ba[1])
+    return msg
 
 def getPort():
     ports = serial.tools.list_ports.comports()
@@ -19,23 +32,41 @@ def getPort():
 portName = getPort()
 print(portName)
 
-
-
 try:
     ser = serial.Serial(port=portName, baudrate=9600)
     print("Open successfully")
 except:
     print("Can not open the port")
 
-relay1_ON  = [2, 6, 0, 0, 0, 255, 185, 91]
-relay1_OFF = [2, 6, 0, 0, 0, 0, 137, 249]
-def setDevice1(state):
-    if state == True:
-        ser.write(relay1_ON)
-    else:
-        ser.write(relay1_OFF)
+
+relay1_ON = [2, 6, 0, 0, 0, 255]
+relay1_OFF = [2, 6, 0, 0, 0, 0]
+
+relay2_ON = [3, 6, 0, 0, 0, 255]
+relay2_OFF = [3, 6, 0, 0, 0, 0]
+
+relay3_ON = [4, 6, 0, 0, 0, 255]
+relay3_OFF = [4, 6, 0, 0, 0, 0]
+
+def setDeviceON(id):
+    if id == 1:
+        ser.write(addModbusCrc(relay1_ON))
+    elif id == 2:
+        ser.write(addModbusCrc(relay2_ON))
+    elif id == 3:
+        ser.write(addModbusCrc(relay3_ON))
     time.sleep(1)
-    # print(serial_read_data(ser))
+    print(serial_read_data(ser))
+
+def setDeviceOFF(id):
+    if id == 1:
+        ser.write(addModbusCrc(relay1_OFF))
+    elif id == 2:
+        ser.write(addModbusCrc(relay2_OFF))
+    elif id == 3:
+        ser.write(addModbusCrc(relay3_OFF))
+    time.sleep(1)
+    print(serial_read_data(ser))
 
 
 def serial_read_data(ser):
@@ -53,25 +84,37 @@ def serial_read_data(ser):
     return 0
 
 
-soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
+soil_temperature =[1, 3, 0, 6, 0, 1]
 def readTemperature():
     serial_read_data(ser)
-    ser.write(soil_temperature)
+    ser.write(addModbusCrc(soil_temperature))
     time.sleep(1)
     return serial_read_data(ser)
 
-soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
+soil_moisture = [1, 3, 0, 7, 0, 1]
 def readMoisture():
     serial_read_data(ser)
-    ser.write(soil_moisture)
+    ser.write(addModbusCrc(soil_moisture))
     time.sleep(1)
     return serial_read_data(ser)
 
 while True:
-    setDevice1(True)
-    time.sleep(2)
-    setDevice1(False)
-    time.sleep(2)
+    print("TEST ACTUATOR")
+    setDeviceON(1)
+    time.sleep(1)
+    setDeviceOFF(1)
+    time.sleep(1)
+
+    setDeviceON(2)
+    time.sleep(1)
+    setDeviceOFF(2)
+    time.sleep(1)
+
+    setDeviceON(3)
+    time.sleep(1)
+    setDeviceOFF(3)
+    time.sleep(1)
+
     print("TEST SENSOR")
     print(readMoisture())
     time.sleep(1)
